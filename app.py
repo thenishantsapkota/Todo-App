a99112b5239060c0edc9bd369f3b52fad07ce37e
Nishant Sapkota <snishant306@gmail.com>
2021-09-28 15:30:09 +0545

    feat: Adds files for the Todo App

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect
+from flask_pymongo import PyMongo
+from datetime import date
+from bson import ObjectId
+
+app = Flask(__name__)
+app.config[
+    "MONGO_URI"
+] = "mongodb+srv://nishant:decoder123J@cluster0.dhgkx.mongodb.net/todo_db?retryWrites=true&w=majority"
+
+mongo = PyMongo()
+mongo.init_app(app)
+
+
+@app.post("/")
+def add_todo():
+    tasks_collection = mongo.db.todoapp
+    new_task = request.form["content"]
+    data = {"content": new_task, "date_created": str(date.today()), "complete": False}
+    tasks_collection.insert_one(data)
+    return redirect("/")
+
+
+@app.get("/")
+def index():
+    tasks_collection = mongo.db.todoapp
+    tasks = tasks_collection.find()
+    return render_template("index.html", tasks=tasks)
+
+
+@app.get("/update/<oid>")
+def update_get(oid):
+    tasks_collection = mongo.db.todoapp
+    task = tasks_collection.find_one({"_id": ObjectId(oid)})
+    return render_template("update.html", task=task)
+
+
+@app.post("/update/<oid>")
+def update_post(oid):
+    tasks_collection = mongo.db.todoapp
+    content = request.form["content"]
+    tasks_collection.update_one({"_id": ObjectId(oid)}, {"$set": {"content": content}})
+    return redirect("/")
+
+
+@app.route('/delete/<oid>')
+def delete(oid):
+    tasks_collection = mongo.db.todoapp
+    tasks_collection.delete_one({'_id': ObjectId(oid)})
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# source C:/Users/snish/AppData/Local/pypoetry/Cache/virtualenvs/todo_app-Xsaj7QwE-py3.9/Scripts/activate
