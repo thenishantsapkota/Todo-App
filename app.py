31d1d0e52e6bdefa0c75c417fb55f92909d98918
Nishant Sapkota <snishant306@gmail.com>
2021-09-28 20:06:30 +0545

    Hotfixes

--- app.py
+++ app.py
@@ -6,15 +6,16 @@ from bson import ObjectId
 app = Flask(__name__)
 app.config[
     "MONGO_URI"
-] = "Your URI Here"
+] = "mongodb+srv://nishant:decoder123J@cluster0.dhgkx.mongodb.net/todo_db?authSource=admin&replicaSet=atlas-r52uxo-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
 
 mongo = PyMongo()
 mongo.init_app(app)
 
+tasks_collection = mongo.db.todoapp
+
 
 @app.post("/")
 def add_todo():
-    tasks_collection = mongo.db.todoapp
     new_task = request.form["content"]
     data = {"content": new_task, "date_created": str(date.today()), "complete": False}
     tasks_collection.insert_one(data)
@@ -23,32 +24,38 @@ def add_todo():
 
 @app.get("/")
 def index():
-    tasks_collection = mongo.db.todoapp
     tasks = tasks_collection.find()
     return render_template("index.html", tasks=tasks)
 
 
 @app.get("/update/<oid>")
-def update_get(oid):
-    tasks_collection = mongo.db.todoapp
+def update_get(oid:str):
     task = tasks_collection.find_one({"_id": ObjectId(oid)})
     return render_template("update.html", task=task)
 
 
 @app.post("/update/<oid>")
-def update_post(oid):
-    tasks_collection = mongo.db.todoapp
+def update_post(oid:str):
+    
     content = request.form["content"]
     tasks_collection.update_one({"_id": ObjectId(oid)}, {"$set": {"content": content}})
     return redirect("/")
 
 
 @app.route('/delete/<oid>')
-def delete(oid):
-    tasks_collection = mongo.db.todoapp
+def delete(oid:str):
+    
     tasks_collection.delete_one({'_id': ObjectId(oid)})
     return redirect('/')
 
+
+@app.route('/complete/<oid>')
+def mark_complete(oid:str):
+    
+    task = tasks_collection.find_one({'_id': ObjectId(oid)})
+    task["complete"] = True
+    tasks_collection.save(task)
+    return redirect ('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
